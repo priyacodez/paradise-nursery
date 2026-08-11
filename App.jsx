e4ddab63774476e95d18bfaabeae83d08ebce13b
priya <priyagopal1104@gmail.com>
2026-08-11 14:38:14 +0530

    Update App.jsx

--- App.jsx
+++ App.jsx
@@ -1,48 +1,28 @@
 
 import React from "react";
-import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
+import { Link } from "react-router-dom";
 
-import AboutUs from "./AboutUs";
-import ProductList from "./ProductList";
-import Cart from "./Cart";
-import "./App.css";
-
-function Home() {
+function App() {
   return (
     <div className="landing-page">
+
       <div className="landing-content">
-        <h1>Welcome to Paradise Nursery</h1>
+        <h1>Paradise Nursery</h1>
 
-        <p>Bring nature into your home.</p>
-
-        <p className="landing-description">
-          Discover our beautiful collection of indoor and outdoor plants.
-          Shop your favorite plants and bring a touch of greenery to your
-          space.
+        <p>
+          Welcome to Paradise Nursery!
+          <br />
+          Discover beautiful plants for your home and garden.
         </p>
 
-        <Link to="/plants">
+        <Link to="/products">
           <button className="get-started-button">
             Get Started
           </button>
         </Link>
       </div>
-    </div>
-  );
-}
 
-function App() {
-  return (
-    <Router>
-      <div className="App">
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/about" element={<AboutUs />} />
-          <Route path="/plants" element={<ProductList />} />
-          <Route path="/cart" element={<Cart />} />
-        </Routes>
-      </div>
-    </Router>
+    </div>
   );
 }
 
